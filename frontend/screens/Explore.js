0404e4dfe43eba4fef8e62cbafff8c8f27afe6d3
b09217es <eduard.stoica@student.manchester.ac.uk>
2021-03-25 00:52:51 +0200

    swipe cards implemented

--- frontend/screens/Explore.js
+++ frontend/screens/Explore.js
@@ -1,18 +1,249 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { Component, useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  PanResponder,
+  Dimensions,
+  Animated,
+} from "react-native";
 
-export default function Explore({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to our 1Night App</Text>
-    </View>
-  );
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const users = [
+  {
+    name: "Elena",
+    age: 21,
+    distance: 58,
+    image: require("../assets/1.png"),
+  },
+  {
+    name: "George",
+    age: 30,
+    distance: 29,
+    image: require("../assets/2.png"),
+  },
+  {
+    name: "Minodora",
+    age: 18,
+    distance: 2,
+    image: require("../assets/3.png"),
+  },
+  {
+    name: "Edi",
+    age: 19,
+    distance: 100,
+    image: require("../assets/4.png"),
+  },
+  {
+    name: "Andreea",
+    age: 23,
+    distance: 10,
+    image: require("../assets/5.png"),
+  },
+];
+export default class Explore extends Component {
+  constructor() {
+    super();
+    this.position = new Animated.ValueXY();
+    this.state = {
+      currentIndex: 0,
+    };
+    this.rotate = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: ["-10deg", "0deg", "10deg"],
+      extrapolate: "clamp",
+    });
+    this.rotateAndTranslate = {
+      transform: [
+        {
+          rotate: this.rotate,
+        },
+        ...this.position.getTranslateTransform(),
+      ],
+    };
+    this.likeOpacity = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [0, 0, 1],
+      extrapolate: "clamp",
+    });
+    this.nopeOpacity = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0, 0],
+      extrapolate: "clamp",
+    });
+    this.nextCardOpacity = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0, 1],
+      extrapolate: "clamp",
+    });
+    this.nextCardScale = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
+      outputRange: [1, 0.8, 1],
+      extrapolate: "clamp",
+    });
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderMove: (evt, gestureState) => {
+        this.position.setValue({ x: gestureState.dx, y: gestureState.dy });
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        if (gestureState.dx > 120) {
+          Animated.spring(this.position, {
+            toValue: { x: SCREEN_WIDTH + 100, y: gestureState.dy },
+            useNativeDriver: true,
+          }).start(() => {
+            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
+              this.position.setValue({ x: 0, y: 0 });
+            });
+          });
+        } else if (gestureState.dx < -120) {
+          Animated.spring(this.position, {
+            toValue: { x: -SCREEN_WIDTH - 100, y: gestureState.dy },
+            useNativeDriver: true,
+          }).start(() => {
+            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
+              this.position.setValue({ x: 0, y: 0 });
+            });
+          });
+        } else {
+          Animated.spring(this.position, {
+            toValue: { x: 0, y: 0 },
+            friction: 4,
+            useNativeDriver: true,
+          }).start();
+        }
+      },
+    });
+  }
+
+  renderUsers = () => {
+    return users
+      .map((user, key) => {
+        if (key < this.state.currentIndex) {
+          return null;
+        } else if (key == this.state.currentIndex) {
+          return (
+            <Animated.View
+              {...this.panResponder.panHandlers}
+              key={key}
+              style={[
+                this.rotateAndTranslate,
+                {
+                  height: "100%",
+                  width: SCREEN_WIDTH,
+                  padding: 10,
+                  position: "absolute",
+                },
+              ]}
+            >
+              <Animated.View
+                style={{
+                  opacity: this.likeOpacity,
+                  transform: [{ rotate: "-30deg" }],
+                  position: "absolute",
+                  top: 50,
+                  left: 40,
+                  zIndex: 1000,
+                }}
+              >
+                <Text
+                  style={{
+                    borderWidth: 1,
+                    borderColor: "green",
+                    color: "green",
+                    fontSize: 32,
+                    fontWeight: "800",
+                    padding: 10,
+                    borderRadius: 10,
+                  }}
+                >
+                  1 Night
+                </Text>
+              </Animated.View>
+              <Animated.View
+                style={{
+                  opacity: this.nopeOpacity,
+                  transform: [{ rotate: "30deg" }],
+                  position: "absolute",
+                  top: 50,
+                  right: 40,
+                  zIndex: 1000,
+                }}
+              >
+                <Text
+                  style={{
+                    borderWidth: 1,
+                    borderColor: "red",
+                    color: "red",
+                    fontSize: 32,
+                    fontWeight: "800",
+                    padding: 10,
+                    borderRadius: 10,
+                  }}
+                >
+                  No Night
+                </Text>
+              </Animated.View>
+              <Image
+                style={{
+                  flex: 1,
+                  height: null,
+                  width: null,
+                  resizeMode: "cover",
+                  borderRadius: 20,
+                }}
+                source={user.image}
+              />
+            </Animated.View>
+          );
+        } else {
+          return (
+            <Animated.View
+              key={key}
+              style={{
+                opacity: this.nextCardOpacity,
+                transform: [{ scale: this.nextCardScale }],
+                height: SCREEN_HEIGHT - 120,
+                width: SCREEN_WIDTH,
+                padding: 10,
+                position: "absolute",
+              }}
+            >
+              <Image
+                style={{
+                  flex: 1,
+                  height: null,
+                  width: null,
+                  resizeMode: "cover",
+                  borderRadius: 20,
+                }}
+                source={user.image}
+              />
+            </Animated.View>
+          );
+        }
+      })
+      .reverse();
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ height: 70 }} />
+        <View style={{ flex: 1 }}>{this.renderUsers()}</View>
+        <View style={{ height: 60 }} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "gray",
+  },
+  card: {
+    flex: 0.9,
   },
 });
